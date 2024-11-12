fb39a66d1b97efbf80e39d2a657d747d9d4e5b3d
AIIA\fabian <mitsalf1@gmail.com>
2024-11-12 11:41:40 +0700

    success page

--- resources/views/emails/success.blade.php
+++ resources/views/emails/success.blade.php
@@ -1,55 +1,34 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 
 <head>
-    <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:400,400i,700,900&display=swap" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Email Verified</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
-<style>
-    body {
-        text-align: center;
-        padding: 40px 0;
-        background: #EBF0F5;
-    }
-
-    h1 {
-        color: #88B04B;
-        font-family: "Nunito Sans", "Helvetica Neue", sans-serif;
-        font-weight: 900;
-        font-size: 40px;
-        margin-bottom: 10px;
-    }
-
-    p {
-        color: #404F5E;
-        font-family: "Nunito Sans", "Helvetica Neue", sans-serif;
-        font-size: 20px;
-        margin: 0;
-    }
-
-    i {
-        color: #9ABC66;
-        font-size: 100px;
-        line-height: 200px;
-        margin-left: -15px;
-    }
-
-    .card {
-        background: white;
-        padding: 60px;
-        border-radius: 4px;
-        box-shadow: 0 2px 3px #C8D0D8;
-        display: inline-block;
-        margin: 0 auto;
-    }
-</style>
 
 <body>
-    <div class="card">
-        <div style="border-radius:200px; height:200px; width:200px; background: #F8FAF5; margin:0 auto;">
-            <i class="checkmark">✓</i>
+    <div class="vh-100 d-flex justify-content-center align-items-center">
+        <div class="col-md-4">
+            <div class="border border-3 border-success"></div>
+            <div class="card  bg-white shadow p-5">
+                <div class="mb-4 text-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="text-success" width="75" height="75"
+                        fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16">
+                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                        <path
+                            d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z" />
+                    </svg>
+                </div>
+                <div class="text-center">
+                    <h1>Thank You !</h1>
+                    <p>Your email has been <strong>Verified</strong></p>
+                    <a href="{{ route('login') }}" class="btn btn-outline-success mt-3">Back Home</a>
+                </div>
+            </div>
         </div>
-        <h1>Success</h1>
-        <p>Your Email has been <strong>Verified</strong></p>
-        <a href="{{ route('login') }}" class="btn btn-info mt-4">Back to Login</a>
     </div>
 </body>
 
